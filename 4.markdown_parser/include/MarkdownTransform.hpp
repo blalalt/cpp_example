2d76c27a7c35a2b4a699f53e39b0059cc0e867ad
blalalt <blalalt@163.com>
2019-11-24 09:48:53 +0800

    4

--- 4.markdown_parser/include/MarkdownTransform.hpp
+++ 4.markdown_parser/include/MarkdownTransform.hpp
@@ -4,7 +4,7 @@
 #include <string>
 #include <vector>
 #include <map>
-
+#include <utility>
 
 class MarkDownTransform {
 public:
@@ -17,7 +17,10 @@ public:
 
 private:
     // Token 枚举 和 html 元素
-    std::map<std::string, std::pair<std::string, std::string>> ht;
+    std::map<std::string, std::pair<std::string, std::string>> ht {
+        {"nul", std::make_pair("", "")},
+        
+    };
     
     // markdown 的内容和目录
     std::string content, TOC; 
--- 4.markdown_parser/src/MarkdownTransform.cc
+++ 4.markdown_parser/src/MarkdownTransform.cc
@@ -1,7 +1,4 @@
 #include <MarkdownTransform.hpp>
 
 MarkDownTransform::MarkDownTransform(const std::string &filename) {
-    this->ht {"nul": ss_p("", ""), 
-    
-    };
 }

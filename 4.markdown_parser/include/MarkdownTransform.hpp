5485de7cbbacb55c82fe1ad9bd6b63579a993fce
blalalt <blalalt@163.com>
2019-11-26 08:29:34 +0800

    4

--- 4.markdown_parser/include/MarkdownTransform.hpp
+++ 4.markdown_parser/include/MarkdownTransform.hpp
@@ -5,6 +5,18 @@
 #include <vector>
 #include <map>
 #include <utility>
+#include <new>
+
+// mkdown 和 html具有类似的dom树结构，因此我们需要先实现一个树结构
+struct Cnode {
+    // 保存目录的结构
+    std::vector<Cnode>
+};
+
+struct node {
+    // 保存正文内容的结构
+
+};
 
 class MarkDownTransform {
 public:
@@ -23,7 +35,22 @@ private:
         {"href", std::make_pair("", "")},
         {"ul", std::make_pair("<ul>", "</ul>")},
         {"ol", std::make_pair("<ol>", "</ol>")},
-        {"li", std::make_pair("<li>", "</li>")}
+        {"li", std::make_pair("<li>", "</li>")},
+        {"em", std::make_pair("<em>", "</em>")},
+        {"strong", std::make_pair("<strong>", "</strong>")},
+        {"hr", std::make_pair("<hr color=#CCCCCC size=1 />", "")},
+        {"br", std::make_pair("<br />", "")},
+        {"br", std::make_pair("<br />", "")},
+        {"img", std::make_pair("", "")},
+        {"quote", std::make_pair("<blockquote>", "</blockquote>")},
+        {"h1", std::make_pair("<h1>", "</h1>")},
+        {"h2", std::make_pair("<h2>", "</h2>")},
+        {"h3", std::make_pair("<h3>", "</h3>")},
+        {"h4", std::make_pair("<h4>", "</h4>")},
+        {"h5", std::make_pair("<h5>", "</h5>")},
+        {"h6", std::make_pair("<h6>", "</h6>")},
+        {"blockcode", std::make_pair("<pre><code>", "</code>")},
+        {"code", std::make_pair("<code>", "</code>")},
     };
     
     // markdown 的内容和目录
@@ -32,4 +59,4 @@ private:
 
 
 
-#endif __MARKDOWNTRANSFORM
+#endif ___MARKDOWNTRANSFORM
